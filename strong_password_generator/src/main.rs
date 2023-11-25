3e6b295cedee7255293de7ad03110d34b15bb33d
Emma <emmanuelponciano@gmail.com>
2023-11-25 15:39:18 -0500

    fix

--- strong_password_generator/src/main.rs
+++ strong_password_generator/src/main.rs
@@ -2,6 +2,13 @@
 use rand::Rng;
 
 fn main() {
-    let tuple = rand::random::<(f64, char)>();
-    println!("{:?}", tuple);
+    let password = generate_password(16);
+    println!("Contraseña generada: {}", password);
+}
+
+fn generate_password(length: usize) -> String {
+    let mut rng = rand::thread_rng();
+    (0..length)
+        .map(|_| rng.gen_range(32..=127) as u8 as char)
+        .collect()
 }
